f1a3caf7d368aa9df80e9d1482c90dba9f0a2589
Stephen Jenkins <sejgit@github.com>
2026-05-24 19:08:30 -0400

    Remove debug logging - commands working successfully

--- nodes/Shade.py
+++ nodes/Shade.py
@@ -53,11 +53,9 @@ class Shade(udi_interface.Node):
 
         # For TaHoma, sid is the deviceURL string
         self.sid = sid
-        LOGGER.info(f"Shade.__init__: address={address}, sid={sid}, type={type(sid)}")
         # TaHoma device URLs can be in various formats:
         # io://, rts://, zigbee://, internal://, etc.
         self.device_url = sid if isinstance(sid, str) and "://" in sid else None
-        LOGGER.info(f"Shade.__init__: device_url={self.device_url}")
 
         self.tiltCapable = [1, 2, 4, 5, 9, 10]
         self.tiltOnly90Capable = [1, 9]
